554b7bb5aab982f786c28dae460d17865c76ebd8
Manish <ranjan.manish421@gmail.com>
2023-06-09 18:37:34 +0530

    removing unused variable

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,6 @@ const logger = require("../logger");
 
 const { DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_HOST } = require("../env");
 
-const DB_URL_STRING = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 async function connect() {
   await mongoose.connect(
     `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
